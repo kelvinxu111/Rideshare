b721718ad066b950390efb38e9305bcba99cad7d
kelvinxu111 <kelvinxu_888@hotmail.com>
2020-11-19 23:42:36 -0500

    Changed project structure

--- Rideshare/src/main/java/com/example/Rideshare/RideshareApplication.java
+++ Rideshare/src/main/java/com/example/Rideshare/RideshareApplication.java
@@ -1,23 +1,20 @@
 package com.example.Rideshare;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.example.Rideshare.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages = {"com/example/Rideshare/model"})
 public class RideshareApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(RideshareApplication.class);
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		SpringApplication.run(RideshareApplication.class, args);
 
-        SpringApplication.run(RideshareApplication.class, args);
-
-    }
+	}
 
 }
