42fe4da8dc1e4cb88a5ffcec4721aecf0d688fb3
Kyle Li <kyleheli888@gmail.com>
2020-11-19 22:57:07 -0500

    Added controller and modified project structure

--- Rideshare/src/main/java/com/example/Rideshare/RideshareApplication.java
+++ Rideshare/src/main/java/com/example/Rideshare/RideshareApplication.java
@@ -1,20 +1,23 @@
 package com.example.Rideshare;
-import model.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EntityScan(basePackages = {"model"})
+@EntityScan(basePackages = {"com/example/Rideshare/model"})
 public class RideshareApplication {
 
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(RideshareApplication.class);
 
-		SpringApplication.run(RideshareApplication.class, args);
+    public static void main(String[] args) {
 
-	}
+        SpringApplication.run(RideshareApplication.class, args);
+
+    }
 
 }
