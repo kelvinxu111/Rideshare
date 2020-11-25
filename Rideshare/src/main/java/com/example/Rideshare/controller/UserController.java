a4fef0b5a37169526e2da26ea241a88f7125eda0
kelvinxu111 <kelvinxu_888@hotmail.com>
2020-11-25 12:59:37 -0500

    Added controller methods for validating login info, activating user accounts, and amending createUser. Also added precursor methods to UserRepository for later features.

--- Rideshare/src/main/java/com/example/Rideshare/controller/UserController.java
+++ Rideshare/src/main/java/com/example/Rideshare/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.example.Rideshare.controller;
 
 import com.example.Rideshare.model.User;
+import com.google.common.hash.Hashing;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.Rideshare.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,7 +19,22 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+    @GetMapping("/users/login")
+    public ResponseEntity checkLoginUser(@RequestBody String username, String password){
+        try{
+            User validateUser = userRepository.findByUserName(username);
+            String hashedPassword = Hashing.sha256().hashString(password, StandardCharsets.UTF_8).toString();
+            if (validateUser.getPassword()==hashedPassword){
+                return new ResponseEntity<User>(validateUser,HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity(null,HttpStatus.BAD_REQUEST);
+            }
 
+        }catch (Exception e){
+            return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     @GetMapping("/users")
     public ResponseEntity getAllUsers(@RequestParam(required = false) Map<String, String> queryParams) {
         try {
@@ -38,20 +55,47 @@ public class UserController {
     }
 
     @GetMapping("/users/{userID}")
-    public ResponseEntity<User> getUserById(@PathVariable("userID") String userID) {
-        return null;
-    }
+    public ResponseEntity getUserById(@PathVariable("userID") String userID) {
+        try{
+            User findUser = userRepository.findByUserID((UUID.fromString(userID)));
+            if (findUser!=null){
+                return new ResponseEntity<User>(findUser, HttpStatus.OK);
+            }else{
+                return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<Exception>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
+
+    }
+    @PostMapping("/users/{userID}")
+    public ResponseEntity activateUser(@PathVariable("userID") String userID){
+        try{
+            User findUser = userRepository.findByUserID((UUID.fromString(userID)));
+            if (findUser!=null){
+                findUser.setActivated(true);
+                userRepository.save(findUser);
+                return new ResponseEntity<User>(findUser, HttpStatus.OK);
+            }else{
+                return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<Exception>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     @PostMapping("/users")
     public ResponseEntity createUser(@RequestBody User user) {
         try {
+            user.setPassword(Hashing.sha256().hashString(user.getPassword(), StandardCharsets.UTF_8).toString());
+            user.setCreated(LocalDateTime.now());
             User u = userRepository.save(user);
             return new ResponseEntity<User>(u, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    //TODO: figure out delete later, after other objects are modelled
     @DeleteMapping("/users/{userID}")
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable("userID") String userID) {
         return null;
--- Rideshare/src/main/java/com/example/Rideshare/repository/UserRepository.java
+++ Rideshare/src/main/java/com/example/Rideshare/repository/UserRepository.java
@@ -2,12 +2,20 @@ package com.example.Rideshare.repository;
 
 import com.example.Rideshare.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    public Optional<User> findByUserID(UUID uuid);
+    public User findByUserID(UUID uuid);
     public User findByUserName(String username);
     public User findByEmail(String email);
+    public void deleteByUserID(UUID uuid);
+    //Don't think this is necessary, but will need to learn
+    //To delete related entities
+    @Modifying
+    @Query("update User u set u.isActivated=?1 where u.userID=?2")
+    public void setUserIsActivatedByUserID(boolean activated,UUID uuid);
 }
